dc4e1fb6fa79792f98c7c7679c50f12b0e8f44c5
Justdo2t <54635114+gkswn911@users.noreply.github.com>
2021-12-20 17:07:57 +0900

    Add Day3 HanJu

---
+++ Level 1/Day 04/hanju_Day4.js
@@ -0,0 +1,25 @@
+function solution(a, b) {
+    
+    var sum = 0;
+    
+    if(a > b)
+    {
+        for(let i = a; i >= b; i--)
+        {
+            sum += i;        
+        }
+        return sum;
+    }
+    else if(a < b)
+    {
+        for(let i = b; i >= a; i--)
+        {
+            sum += i;        
+        }
+        return sum;
+    }
+    else
+    {
+        return a;
+    }
+}
