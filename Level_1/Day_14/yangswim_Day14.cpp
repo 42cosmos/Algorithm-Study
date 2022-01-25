04c30dcb890c5cbe99bbebb767b369bf93183bc2
tasddc1226 <tasddc@naver.com>
2022-01-25 20:56:54 +0900

    Add Day14 Swim

---
+++ Level_1/Day_14/yangswim_Day14.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <math.h>
+#define MAX 1000001
+
+using namespace std;
+
+// 1이면 소수, 0이면 소수 아님!
+int not_prime_nums[MAX] = {0, 1, 0, }; 
+
+int solution(int n) {
+    int answer = 0;
+    
+    for (int p = 2; p <= n; p++) {
+        if (not_prime_nums[p] == 0) {
+            // p의 i 배수를 모두 소수 처리
+            for (int i = 2; p*i <= n; i++){
+                not_prime_nums[p*i] = 1;
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        if (not_prime_nums[i] == 0)
+            answer++;
+    }
+    
+    return answer;
+}
